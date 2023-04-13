c72e19af853fc3eeb316e05e2c3cd91c66eb7fca
[Juan A.] <juan-aga@student.42.malaga.com>
2023-04-14 01:12:37 +0200

    ADD: sos.py

---
+++ ex08/sos.py
@@ -0,0 +1,28 @@
+import sys
+
+morse = {'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....',
+        'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 
+        'O':'---', 'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-',
+        'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..',
+        '1':'.----', '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....',
+        '7':'--...', '8':'---..', '9':'----.', '0':'-----', " ":"/"}
+
+def error ():
+    print("ERROR")
+    exit()
+
+if  __name__ == "__main__":
+    mes = ""
+    for i in range(1, len(sys.argv), 1):
+        if " " in sys.argv[i]:
+            for c in sys.argv[i].split():
+                mes += c + " "
+        else:
+            mes += sys.argv[i] + " "
+    for word in mes.split():
+       if not word.isalnum():
+            error()
+    encoded = ""
+    for char in mes:
+        encoded += (morse[char.upper()] + " ")    
+    print(encoded[:-2])
