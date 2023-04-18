c4fe633c8f1f00722f1dc1095911f59418160237
juan-aga <juan-aga@student.42malaga.com>
2023-04-18 20:05:19 +0200

    FIX: ex04

--- ex04/operations.py
+++ ex04/operations.py
@@ -17,11 +17,16 @@ def operations (num1, num2):
         print("Remainder:  %i"%(num1 % num2))
 
 if __name__=='__main__':
-    if len(sys.argv) < 3:
-        print("Usage: python operations.py <number1> <number2>\nExample:\n\tpython operations.py 10 3")
-    elif len(sys.argv) > 3:
-        print("too many arguments")
-    elif sys.argv[1].isnumeric() and sys.argv[2].isnumeric():
-        operations(sys.argv[1], sys.argv[2])
-    else:
-        print("only integers")
+	if len(sys.argv) < 3:
+		print("Usage: python operations.py <number1> <number2>\nExample:\n\tpython operations.py 10 3")
+	elif len(sys.argv) > 3:
+		print("too many arguments")
+	else:
+		try:
+			operations(int(sys.argv[1]), int(sys.argv[2]))
+		except:
+			print("only integers")
+#	elif sys.argv[1].isnumeric() and sys.argv[2].isnumeric():
+#        operations(sys.argv[1], sys.argv[2])
+#    else:
+#        print("only integers")
