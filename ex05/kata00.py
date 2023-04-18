440d12778897d124c4220d902f3df1a590c517b4
juan-aga <juan-aga@student.42malaga.com>
2023-04-18 20:17:14 +0200

    FIX: ex05

--- ex05/kata00.py
+++ ex05/kata00.py
@@ -1,6 +1,6 @@
 kata = (19,42,21)
 
-to_print = "The {size} numbers are:".format(size = len(kata))
+to_print = "The {size} numbers are: ".format(size = len(kata))
 for number in kata:
-    to_print += ", " + str(number)
-print(to_print)
+    to_print += str(number) + ", "
+print(to_print[:-2])
--- ex05/kata02.py
+++ ex05/kata02.py
@@ -1,4 +1,3 @@
 kata = (2019, 9, 25, 3, 30)
 
-print("{m:0>2}/{d:0>2}/{y:0>2} {h:0>2}:{mn:0>2}".format(y = kata[0], m = kata[1], d = kata[2], h = kata[3], mn = kata[4]))
-
+print("{m:0>2}/{d:0>2}/{y:0>4} {h:0>2}:{mn:0>2}".format(y = kata[0], m = kata[1], d = kata[2], h = kata[3], mn = kata[4]))
