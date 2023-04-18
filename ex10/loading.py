b95538635fa5b37b61e1e1bcc0b880af58288670
juan-aga <juan-aga@student.42malaga.com>
2023-04-18 21:45:59 +0200

    FIX: ex10

--- ex10/loading.py
+++ ex10/loading.py
@@ -4,22 +4,26 @@ import string
 def ft_progress(lst):
     t = time()
     barSize = 60
-    complete = int(lst[-1]) + 1
-    print("\rETA: 0.00s [{p:>3}%%][{s:>40}] 0/%i | elapsed time 0.00s".format(p="0", s = "")%(complete), end="")
-    for n in lst: 
-        yield n
-        progress = int(round(barSize * (n+1)) / complete) 
+    last = int(lst[-1])
+    l = 1
+    if last < 0:
+       l *= -1
+    complete = last + l
+    total =(abs(complete - l))
+    for n, m in enumerate(lst,1): 
+        yield m
+        progress = int(round(barSize * (n - 1)) / total)
         bar = '=' * progress + '>'# + ' ' * (barSize - progress)
         per = int(100 * (progress) / barSize)
         elapsed = time() - t
-        eta = elapsed / (n + 1) * (complete - n -1)
-        print("\rETA: {t:.2f}s [{p:>3}%%][{s:<{b}}] {i:>{l}}/%i | elapsed time {e:.2f}s".format(s = bar, b = barSize + 1,t=eta, p= per, e = elapsed, i = n+1, l = len(str(complete)))%(complete), end="")
+        eta = elapsed / n * (total - n + 1)
+        print("\rETA: {t:.2f}s [{p:>3}%%][{s:<{b}}] {i:>{l}}/%i | elapsed time {e:.2f}s".format(s = bar, b = barSize + 1,t=eta, p= per, e = elapsed, i = m + l, l = len(str(complete)))%(complete), end="")
 
 if __name__ == "__main__":
-    listy = range(3333)
+    listy = range(300)
     ret = 0
     for elem in ft_progress(listy):
         ret += elem
-        sleep(0.005)
+        sleep(0.01)
     print()
     print(ret)
